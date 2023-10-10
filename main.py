a34981003b64377a31d8dfa4999e8a746625ac9b
lFlySparkl <dmitrii.barasii@gmail.com>
2023-10-10 04:15:25 +0300

    update

--- main.py
+++ main.py
@@ -1,71 +1,31 @@
-from datetime import date
-from datetime import datetime
+from datetime import timedelta, date, datetime
 
 
 def day_change(birthday_arr: list, b_date: date, user, current_date: date):
     get_day = b_date.strftime("%A")
 
-    if b_date.month == 12 and b_date.day == 30 and get_day == "Saturday":
-        b_date = b_date.replace(day=1, month=1, year=current_date.year + 1)
+    if get_day == "Saturday":
+        b_date = b_date + timedelta(days=2)
         user["birthday"] = b_date
         birthday_arr.append(user)
-    elif b_date.month == 12 and b_date.day == 30 and get_day == "Sunday":
-        b_date = b_date.replace(day=1, month=1, year=current_date.year + 1)
-        user["birthday"] = b_date
-        birthday_arr.append(user)
-    elif b_date.month == 12 and b_date.day == 31 and get_day == "Saturday":
-        b_date = b_date.replace(day=2, month=1, year=current_date.year + 1)
-        user["birthday"] = b_date
-        birthday_arr.append(user)
-    elif b_date.month == 12 and b_date.day == 31 and get_day == "Sunday":
-        b_date = b_date.replace(day=1, month=1, year=current_date.year + 1)
+    elif get_day == "Sunday":
+        b_date = b_date + timedelta(days=2)
         user["birthday"] = b_date
         birthday_arr.append(user)
     else:
-        if get_day == "Saturday":
-            if b_date.day == 30:
-                b_date = b_date.replace(day=1, month=current_date.month + 1)
-                user["birthday"] = b_date
-                birthday_arr.append(user)
-            elif b_date.day == 31:
-                b_date = b_date.replace(day=1, month=current_date.month + 1)
-                user["birthday"] = b_date
-                birthday_arr.append(user)
-            else:
-                b_date = b_date.replace(day=b_date.day + 2)
-                user["birthday"] = b_date
-                birthday_arr.append(user)
-        elif get_day == "Sunday":
-            if b_date.day == 30:
-                b_date = b_date.replace(
-                    day=current_date.day + 1, month=current_date.month + 1
-                )
-                user["birthday"] = b_date
-                birthday_arr.append(user)
-            elif b_date.day == 31:
-                b_date = b_date.replace(
-                    day=current_date.day + 1, month=current_date.month + 1
-                )
-                user["birthday"] = b_date
-                birthday_arr.append(user)
-            else:
-                b_date = b_date.replace(day=b_date.day + 1)
-                user["birthday"] = b_date
-                birthday_arr.append(user)
-        else:
-            user["birthday"] = b_date
-            birthday_arr.append(user)
+        user["birthday"] = b_date
+        birthday_arr.append(user)
     return birthday_arr
 
 
 def get_birthdays_per_week(users):
-    # current_date = date.today()
-    current_date = date(2024, 6, 30)
+    current_date = date.today()
     birthday_arr = []
 
     for user in users:
         bd: date = user["birthday"]
         bd = bd.replace(year=current_date.year)
+
 
         if bd.month >= current_date.month:
             if bd.day <= current_date.day and bd.month == current_date.month:
@@ -80,7 +40,14 @@ def get_birthdays_per_week(users):
 
 if __name__ == "__main__":
     users = [
-        {"name": "Jan Koum", "birthday": date(2024, 6, 30)},
+        # {"name": "Jan Koum", "birthday": date(2024, 6, 30)},
+        {"name": "Jan Koum", "birthday": datetime(1976, 1, 1).date()},
+        {"name": "Bill", "birthday": date(1990, 10, 30)},
+        {"name": "Marry", "birthday": date(2000, 12, 30)},
+        {"name": "Jinny", "birthday": date(2000, 12, 31)},
+        {"name": "Jan Koum", "birthday": datetime(1976, 11, 1).date()},
+        {"name": "Jan Koum", "birthday": datetime(1976, 10, 1).date()},
+        {"name": "Jan Koum", "birthday": datetime(1976, 2, 28).date()},
     ]
 
     result: list = get_birthdays_per_week(users)
