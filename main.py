5b980936e4cef65100acb8826362e32aeded0dfe
lFlySparkl <dmitrii.barasii@gmail.com>
2023-10-12 00:54:55 +0300

    update

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ def day_change(birthday_arr: dict, b_date: date, user, current_date: date):
     return birthday_arr
 
 
+def combine_list(list: dict):
+    result = {}
+    for name, date_bd in list.items():
+        date_bd: date = date_bd
+        result.setdefault(date_bd.strftime("%A"), []).append(name)
+    return result
+
+
 def get_birthdays_per_week(users):
     current_date = date.today()
     birthday_arr = {}
@@ -24,8 +32,8 @@ def get_birthdays_per_week(users):
 
     for user in users:
         bd: date = user["birthday"]
-        print(current_date, bd)
-        
+        # print(current_date, bd)
+
         if bd.year > current_date.year:
             day_change(birthday_arr, bd, user, current_date)
         else:
@@ -38,9 +46,9 @@ def get_birthdays_per_week(users):
                         continue
                 else:
                     day_change(birthday_arr, bd, user, current_date)
-            
-    print(birthday_arr)
-    return birthday_arr
+
+    res = combine_list(birthday_arr)
+    return res
 
 
 if __name__ == "__main__":
@@ -53,18 +61,18 @@ if __name__ == "__main__":
         # {"name": "Jan Koum", "birthday": datetime(1976, 11, 1).date()},
         # {"name": "Jan Koum", "birthday": datetime(1976, 10, 1).date()},
         # {"name": "Jan Koum", "birthday": datetime(1976, 2, 28).date()},
-        # 
-        # {'name': 'John', 'birthday': datetime(2023, 12, 27).date()}, 
+        #
+        # {'name': 'John', 'birthday': datetime(2023, 12, 27).date()},
         # {'name': 'Doe', 'birthday': datetime(2023, 12, 29).date()},
         # {'name': 'Alice', 'birthday': datetime(2023, 12, 23).date()},
-        # 
+        #
         # {'name': 'John', 'birthday': datetime(2023, 12, 21).date()},
         # {'name': 'Doe', 'birthday': datetime(2023, 12, 20).date()},
         # {'name': 'Alice', 'birthday': datetime(2021, 1, 1).date()},
-        # 
-        {'name': 'John', 'birthday': datetime(2023, 12, 31).date()},
-        {'name': 'Doe', 'birthday': datetime(2024, 1, 1).date()},
-        {'name': 'Alice', 'birthday': datetime(2023, 12, 29).date()}
+        #
+        {"name": "John", "birthday": datetime(2023, 12, 31).date()},
+        {"name": "Doe", "birthday": datetime(2024, 1, 1).date()},
+        {"name": "Alice", "birthday": datetime(2023, 12, 29).date()},
     ]
 
     result = get_birthdays_per_week(users)
